b1227428f81062c39d01fa58af888d70ee34edcc
yunmee0704 <900704630c@gmail.com>
2024-01-14 18:50:08 +0900

    router-1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';//부트스트랩 css
 import { Container, Nav, Navbar, Row, Col} from 'react-bootstrap'
 import {useState} from 'react'
-import {data} from './data'
+import data from './data'
 import {Routes, Route, Link} from 'react-router-dom'
 
 function App() {
@@ -15,40 +15,64 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">신발가게</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#Cart">Cart</Nav.Link>
+            <Link to="/">Home</Link>
+            <Link to="/detail">Cart</Link>
           </Nav>
         </Container>
       </Navbar>
-
+     
       <Routes>
-        <Route path ="/" element={<div>메인</div>}/>
-        <Route path ="/detail" element={<div>나는 상세페이지</div>}/>
+        <Route path ="/" element={
+        <div>      
+          <div className='main-bg'></div>
+            <Container>
+            <Row>
+            {
+              shoes.map(function(a,i){
+                return(
+                  <Card shoes={shoes[i]}/>
+                )
+              })
+            }
+            </Row>
+            </Container>
+          </div>}/>
+        <Route path ="/detail" element={      
+          <Detail/>
+        }/>
       </Routes>
 
-      <div className='main-bg'></div>
 
-      <Container>
-      <Row>
-        <Col>
-          <img src={process.env.PUBLIC_URL +  '/img/shoes1.jpg'} width="80%" />
-          <h4>상품명</h4>
-          <p>상품정보</p>
-        </Col>
-        <Col>
-        <img src={process.env.PUBLIC_URL +  '/img/shoes2.jpg'} width="80%" />
-          <h4>상품명</h4>
-          <p>상품정보</p>
-        </Col>
-        <Col>
-        <img src={process.env.PUBLIC_URL +  '/img/shoes3.jpg'} width="80%" />
-          <h4>상품명</h4>
-          <p>상품정보</p>
-        </Col>
-      </Row>
-    </Container>
     </div>
   );
+}
+function Card(props){
+  return(
+    <Col>
+      <img src={process.env.PUBLIC_URL +  `/img/shoes${props.shoes.id+1}.jpg`} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </Col>
+  )
+}
+
+function Detail(props){
+  return(
+    <div className="container">
+      <div className="row">
+      <div className="col-md-6">
+        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+      </div>
+      <div className="col-md-6">
+        <h4 className="pt-5">상품명</h4>
+        <p>상품설명</p>
+        <p>120000원</p>
+        <button className="btn btn-danger">주문하기</button> 
+      </div>
+      </div>
+    </div> 
+  )
 }
 
 export default App;
